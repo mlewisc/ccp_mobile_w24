e26745ca6f092b441f722958272118fc36a00f82
Maia <145039692+mlewisc@users.noreply.github.com>
2024-02-11 08:33:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function goToTravelPage() {
+    window.location.href= "travel.html";
+}
+
+function goToPetsPage() {
+    window.location.href= "pets.html";
+}
+
+function goToRecipesPage() {
+    window.location.href= "recipes.html";
+}
